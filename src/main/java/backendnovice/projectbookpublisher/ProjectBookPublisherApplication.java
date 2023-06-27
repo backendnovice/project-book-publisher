001bf01c05ffecfae93fe3279c956d9dad95d342
backendnovice <backendnovice@gmail.com>
2023-06-27 21:37:29 +0900

    :tada: 스프링 프로젝트 생성

---
+++ src/main/java/backendnovice/projectbookpublisher/ProjectBookPublisherApplication.java
@@ -0,0 +1,13 @@
+package backendnovice.projectbookpublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBookPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBookPublisherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backendnovice/projectbookpublisher/ProjectBookPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package backendnovice.projectbookpublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBookPublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
